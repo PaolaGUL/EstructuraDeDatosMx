127eca0d0475818194d3b05d018958fd48ea78a9
PaolaGUL <ext_10220245@ucol.mx>
2022-09-08 19:41:48 -0400

    update

--- vectores/inventarioVectores.js
+++ vectores/inventarioVectores.js
@@ -4,3 +4,5 @@ let codProducto = document.getElementById("codigoP");
 let nombreP = document.getElementById("nombreP");
 let cantidadP = document.getElementById("cantidadP");
 let costo = document.getElementById("costo");
+
+let
