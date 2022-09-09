baebca90c38debf20450614a389c2019061e1883
PaolaGUL <ext_10220245@ucol.mx>
2022-09-09 16:06:22 -0400

    update

--- vectores/inventarioVectores.js
+++ vectores/inventarioVectores.js
@@ -4,3 +4,11 @@
 // let nombreP = document.getElementById("nombreP");
 // let cantidadP = document.getElementById("cantidadP");
 // let costo = document.getElementById("costo");
+
+// declarando el array producto
+
+let producto = [];
+producto[0] = document.getElementById('codigoP');
+producto[1] = document.getElementById('nombreP');
+producto[2] = document.getElementById('cantidadP');
+producto[3] = document.getElementById('costo');
