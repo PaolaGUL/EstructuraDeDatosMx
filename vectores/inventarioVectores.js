b79cc9f4920455d04431d44f8a7f2aed40bd86d9
PaolaGUL <ext_10220245@ucol.mx>
2022-09-19 18:47:57 -0400

    update

--- vectores/inventarioVectores.js
+++ vectores/inventarioVectores.js
@@ -15,8 +15,13 @@
 
 function agregar() {
     let producto = [];
-    producto[0][0] = document.getElementById('codigoP');
-    producto[0][1] = document.getElementById('nombreP');
-    producto[0][2] = document.getElementById('cantidadP');
-    producto[0][3] = document.getElementById('costo');
+    let i = document.getElementById('ingresar');
+    i.parseInt();
+    for (var c = 0; c <= i; i++) {
+        producto[c][0] = document.getElementById('codigoP');
+        producto[c][1] = document.getElementById('nombreP');
+        producto[c][2] = document.getElementById('cantidadP');
+        producto[c][3] = document.getElementById('costo');
+    }
+
 }
