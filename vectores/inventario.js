a0f011b3b83a180c41150c0a758a7adf3292b18d
PaolaGUL <ext_10220245@ucol.mx>
2022-09-19 22:51:17 -0400

    update

--- vectores/inventario.js
+++ vectores/inventario.js
@@ -1,50 +1,58 @@
-const agregar = document.getElementById('agregar');
-let producto = [];
+window.onload = function() {
+    const agregar = document.getElementById('agregar');
+    let producto = [];
 
-function agregarPro() {
-    producto[0] = document.getElementById('codigoP');
-    producto[1] = document.getElementById('nombreP');
-    producto[2] = document.getElementById('cantidadP');
-    producto[3] = document.getElementById('costo');
-    //document.getElementById('demo').innerHTML = "Se ha agregado correctamente";
-    document.write("Se ha agredado correctamente");
-}
-agregar.onclick = agregarPro;
 
-function modificar() {
-    let prob = [];
-    prob[0] = document.getElementById('codigoPro');
-    prob[0].parseInt();
-    producto[0].parseInt();
-    if (producto[0] == porb[0]) {
-        producto[1] = document.getElementById('nombrePro');
-        producto[2] = document.getElementById('cantidadPro');
-        producto[3] = document.getElementById('costoPro');
-        //verificar();
-        document.getElementById('demo2').innerHTML = "Se ha modificado correctamente";
-    } else {
-        document.getElementById('demo2').innerHTML = "No se encontro el producto";
+    function agregarPro() {
+        producto[0] = document.getElementById('codigoP');
+        producto[1] = document.getElementById('nombreP');
+        producto[2] = document.getElementById('cantidadP');
+        producto[3] = document.getElementById('costo');
+        //document.getElementById('demo').innerHTML = "Se ha agregado correctamente";
+        document.write("Se ha agredado correctamente");
     }
-}
+    agregar.onclick = agregarPro;
 
-function eliminar() {
-    let prob = [];
-    prob[0] = document.getElementById('codigoPro');
-    prob[0].parseInt();
-    producto[0].parseInt();
-    if (producto[0] == prob[0]) {
-        producto[1] = '';
-        producto[2] = '';
-        producto[3] = '';
-        document.getElementById('demo2').innerHTML = "Se ha eliminado correctamente";
-    } else {
-        document.getElementById('demo2').innerHTML = "No se encontro el producto";
+    function modificar() {
+        agregarPro();
+        let prob = [];
+        prob[0] = document.getElementById('codigoPro');
+        prob[0].parseInt();
+        producto[0].parseInt();
+        if (producto[0] == porb[0]) {
+            producto[1] = document.getElementById('nombrePro');
+            producto[2] = document.getElementById('cantidadPro');
+            producto[3] = document.getElementById('costoPro');
+            //verificar();
+            document.getElementById('demo2').innerHTML = "Se ha modificado correctamente";
+        } else {
+            document.getElementById('demo2').innerHTML = "No se encontro el producto";
+        }
     }
-}
 
-function mostrar() {
-    var p = document.createElement('p');
-    p = document.write(producto[0] + ' ' + producto[1] + ' ' + producto[2] + ' ' + producto[3]);
-    p.appendChild();
+    function eliminar() {
+        let prob = [];
+        prob[0] = document.getElementById('codigoPro');
+        prob[0].parseInt();
+        producto[0].parseInt();
+        if (producto[0] == prob[0]) {
+            producto[1] = '';
+            producto[2] = '';
+            producto[3] = '';
+            document.getElementById('demo2').innerHTML = "Se ha eliminado correctamente";
+        } else {
+            document.getElementById('demo2').innerHTML = "No se encontro el producto";
+        }
+    }
 
+    function mostrar() {
+        var p = document.createElement('p');
+        p = document.write(producto[0] + ' ' + producto[1] + ' ' + producto[2] + ' ' + producto[3]);
+        p.appendChild();
+
+    }
+
+    function inverso() {
+        document.writeln(producto[3] + ' ' + producto[2] + ' ' + producto[1] + ' ' + producto[0]);
+    }
 }
