0f3b90fa95545530b8a0d97838c67ce62637ac83
PaolaGUL <ext_10220245@ucol.mx>
2022-10-24 12:57:41 -0400

    update

--- inventario/inventarioListas.js
+++ inventario/inventarioListas.js
@@ -1,3 +1,20 @@
 const inventario = document.createElement('div');
 
 // Agregar, buscar, elminiar, insertar, listar, listar inverso
+
+const form = document.createElement('form');
+
+let esp = document.createElement('br');
+let esp1 = document.createElement('br');
+
+form.append(esp);
+form.append(esp1);
+
+// Código
+let lcodigo = document.createElement('label');
+lcodigo.innerText = 'Código: ';
+let codigo = document.createElement('input');
+codigo.id = 'codigoP';
+
+form.append(lcodigo);
+form.append(codigo);
